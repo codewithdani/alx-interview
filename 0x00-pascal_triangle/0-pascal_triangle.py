83b5ed080207afaaf6b5802c70814734ba119dcc
Daniel <daniel@gmail.com>
2023-11-28 19:05:07 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -19,7 +19,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = p_triangle[i - 1]
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
+        new_row = [1] + [prev_row[j] + prev_row[j + 1]
+                         for j in range(len(prev_row) - 1)] + [1]
         p_triangle.append(new_row)
 
     return p_triangle
