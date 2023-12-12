ee02647ca4a50d413fc1b10682383aa9e1285cd3
Daniel <daniel@gmail.com>
2023-12-12 19:06:09 +0300

    documentation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,18 +13,25 @@ def minOperations(n):
     """
     # Base cases
     if n == 0:
+        # If n is 0, no operations are needed.
         return 0
     elif n == 1:
+        # If n is 1, only one operation (Copy All) is needed.
         return 1
 
     # Recursively calculate the minimum number of
     # operations for n-1 and n/2 characters
     min_n_1 = minOperations(n-1)
+    # Store the minimum operations for n-1 characters in a variable.
     min_n_/2 = minOperations(n//2)
+    # Store the minimum operations for n/2 characters in a variable.
+
 
     # Choose the minimum of the two options
     if n % 2 == 0:
+        # If n is even
         return min(min_n_1 + 1, min_n_/2 + 2)
     else:
+        # If n is odd, only Option 1 is possible.
         return min_n_1 + 1
 
